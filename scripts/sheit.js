0bbf610e0ce17c30f1a27d85759bcd0508f5f58a
Austin Pray <austin@austinpray.com>
2015-04-17 09:33:08 -0500

    bad regex

--- scripts/sheit.js
+++ scripts/sheit.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function(robot) {
-  robot.hear(/s{1,}h{1,}e{1,}i{1,}t{1,}/, function(res){
+  robot.hear(/s+?h+?e+?i+?t/ig, function(res) {
     res.send('https://www.youtube.com/watch?v=NOU7MdkwyOk');
   });
 };
